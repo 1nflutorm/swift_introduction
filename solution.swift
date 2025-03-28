840ea746f8445be1af1c89de4eebc4f71cba6f2c
1nflutorm <1nflutorm@mail.ru>
2025-03-28 12:59:53 +0300

    done

---
+++ solution.swift
@@ -0,0 +1,10 @@
+var firstName = Иван;
+var lastName = Волков;
+var age = 21;
+var university = СПБГЭТУ ЛЭТИ;
+var course = 4
+var professionanlInterest = С++ разработка;
+
+var info = Меня зовут (lastName) (firstName), мне (age) лет, я являются студентом (course) курса в институте (university).nПрофессиональные интересы (professionanlInterest);
+
+print(info);
