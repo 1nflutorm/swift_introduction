7806909339e42c37780c6a2a39e80a896579d1c0
1nflutorm <1nflutorm@mail.ru>
2025-03-28 16:22:43 +0300

    done

--- solution.swift
+++ solution.swift
@@ -5,6 +5,6 @@ var university = СПБГЭТУ ЛЭТИ;
 var course = 4
 var professionanlInterest = С++ разработка;
 
-var info = Меня зовут (lastName) (firstName), мне (age) лет, я являются студентом (course) курса в институте (university).nПрофессиональные интересы (professionanlInterest);
+var info = "Меня зовут \(lastName) \(firstName), мне \(age) лет, я являются студентом \(course) курса в институте \(university). \nПрофессиональные интересы \(professionanlInterest)";
 
 print(info);
